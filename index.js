ce3a872327a74f35a74365d257a9220e249749f2
dt <denersonluz@gmail.com>
2023-10-19 19:58:58 -0300

    Adicionando outras alterações ao exercicio-html

--- index.js
+++ index.js
@@ -2,34 +2,36 @@ var whiteMode = window.document.getElementById("claro");
 var darkMode = window.document.getElementById("escuro");
 
 var body = window.document.getElementById("body");
-var sectionOne = window.document.getElementById("sectionOne");
-var sectionTwo = window.document.getElementById("sectionTwo");
+var sectionO = window.document.getElementById("sectionOne");
+var sectionT = window.document.getElementById("sectionTwo");
 
 var classDark = window.document.getElementById("escuro");
 var classWhite = window.document.getElementById("claro");
+
+sectionO.classList.toggle("section");
+sectionT.classList.toggle("section");
+
+classDark.classList.toggle("dk");
+classWhite.classList.toggle("wt");
 
 darkMode.addEventListener("click", () => {
     body.style.background = "black";
     body.style.color = "white";
 
-    sectionOne.style.background = "#3b3a3a";
-    sectionTwo.style.background = "#3b3a3a";
-
-    sectionOne.classList.toggle("section");
-    sectionTwo.classList.toggle("section");
-    classDark.classList.toggle("mode");
+    sectionO.style.background = "#3b3a3a";
+    sectionT.style.background = "#3b3a3a";
 
     darkMode.style.display = "none"
     whiteMode.style.display = "block"
+    whiteMode.style.background = "white"
 });
 
 whiteMode.addEventListener("click", () => {
     body.style.background = "#108787ff";
-    body.style.color = "white";
+    body.style.color = "black";
 
-    sectionOne.style.background = "white";
-    sectionTwo.style.background = "white";
-    classWhite.classList.toggle("mode");
+    sectionO.style.background = "white";
+    sectionT.style.background = "white";
 
     whiteMode.style.display = "none";
     darkMode.style.display = "block";
