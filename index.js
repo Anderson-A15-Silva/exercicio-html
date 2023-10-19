984f718e25656ef2e2ec5260f448fa9d42c1c12c
dt <denersonluz@gmail.com>
2023-10-18 21:30:43 -0300

    Adicionando alterações em exercicio-html

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ var whiteMode = window.document.getElementById("claro");
 var darkMode = window.document.getElementById("escuro");
 
 var body = window.document.getElementById("body");
-var section = window.document.getElementsByClassName("section")
+var sectionOne = window.document.getElementById("sectionOne");
+var sectionTwo = window.document.getElementById("sectionTwo");
+
+var classDark = window.document.getElementById("escuro");
+var classWhite = window.document.getElementById("claro");
 
 darkMode.addEventListener("click", () => {
     body.style.background = "#073939";
     body.style.color = "white";
+
+    sectionOne.style.background = "8d8d8d"
+    sectionTwo.style.background = "8d8d8d"
+    classDark.classList.toggle("mode");
 });
 
 whiteMode.addEventListener("click", () => {
     body.style.background = "#108787ff";
     body.style.color = "white";
+
+    sectionOne.style.background = "white"
+    sectionTwo.style.background = "white"
+    classWhite.classList.toggle("mode");
 });
 
 
